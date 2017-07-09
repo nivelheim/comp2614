51eec342ecf0a2894005cb1cd3901ba61be02808
nivelheim <zrtune@gmail.com>
2017-07-08 21:14:09 -0700

    new classes
    DAO, customer class

---
+++ COMP2614Assign06/COMP2614Assign06/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+/// <summary>
+/// ---Class Customer---
+/// Item class to save all the prroperties of each rows from DB
+/// </summary>
+public class Customer
+{
+    public String CustomerCode { get; set; }
+    public String CompanyName { get; set; }
+    public String Address1 { get; set; }
+    public String Address2 { get; set; }
+    public String City { get; set; }
+    public String Province { get; set; }
+    public String PostalCode { get; set; }
+    public decimal YtdSales { get; set; }
+    public bool? IsCreditHold { get; set; }
+    public String Notes { get; set; }
+
+	public Customer(String cCode, String cName, String add1, String add2, String cty, 
+        String prov, String pCode, decimal sale, bool? cred, String note)
+	{
+        CustomerCode = cCode;
+        CompanyName = cName;
+        Address1 = add1;
+        Address2 = add2;
+        City = cty;
+        Province = prov;
+        PostalCode = pCode;
+        YtdSales = sale;
+        IsCreditHold = cred;
+        Notes = note;
+	}
+}
