21c3c9ef999218d34e4a7eeddc5f75cb7ee09510
nivelheim <zrtune@gmail.com>
2017-07-08 20:07:26 -0700

    assign06
    first commit

---
+++ COMP2614Assign06/COMP2614Assign06/CustomerDao.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class CustomerDao
+{
+	public CustomerDao()
+	{
+	}
+}
---
+++ COMP2614Assign06/COMP2614Assign06/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace COMP2614Assign06
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.skuTextBox = new System.Windows.Forms.TextBox();
+            this.descTextBox = new System.Windows.Forms.TextBox();
+            this.costTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // skuTextBox
+            // 
+            this.skuTextBox.Location = new System.Drawing.Point(88, 75);
+            this.skuTextBox.Name = "skuTextBox";
+            this.skuTextBox.Size = new System.Drawing.Size(100, 21);
+            this.skuTextBox.TabIndex = 0;
+            // 
+            // descTextBox
+            // 
+            this.descTextBox.Location = new System.Drawing.Point(88, 136);
+            this.descTextBox.Name = "descTextBox";
+            this.descTextBox.Size = new System.Drawing.Size(100, 21);
+            this.descTextBox.TabIndex = 1;
+            // 
+            // costTextBox
+            // 
+            this.costTextBox.Location = new System.Drawing.Point(98, 194);
+            this.costTextBox.Name = "costTextBox";
+            this.costTextBox.Size = new System.Drawing.Size(100, 21);
+            this.costTextBox.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(88, 284);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(564, 206);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 528);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.costTextBox);
+            this.Controls.Add(this.descTextBox);
+            this.Controls.Add(this.skuTextBox);
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox skuTextBox;
+        private System.Windows.Forms.TextBox descTextBox;
+        private System.Windows.Forms.TextBox costTextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
